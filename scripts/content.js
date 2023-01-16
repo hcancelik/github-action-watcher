82cc18aa902467041daf17c1741893d9926e1cf9
Can Celik <celik@me.com>
2023-01-16 11:31:47 +0300

    Update to continue checking after refresh

--- scripts/content.js
+++ scripts/content.js
@@ -10,6 +10,8 @@ function setup() {
       <img alt="Watch Actions" id="github-pr-watcher-icon" src='${chrome.runtime.getURL("/images/icon-32.png")}' height='20' style='padding-right: 5px;' /> Watch Actions
      </div>`;
     button.addEventListener("click", () => {
+      chrome.storage.local.set({ [window.location.href]: true });
+
       const img = document.getElementById("github-pr-watcher-icon");
 
       img.src = chrome.runtime.getURL("/images/loading.gif");
@@ -27,6 +29,8 @@ function setup() {
             img.src = chrome.runtime.getURL("/images/icon-32.png");
 
             clearInterval(watchInterval);
+
+            chrome.storage.local.remove(window.location.href);
           }
         });
       }, 1000);
@@ -37,6 +41,12 @@ function setup() {
     if (lastChild) {
       actions.insertBefore(button, lastChild);
     }
+
+    chrome.storage.local.get(window.location.href, function (result) {
+      if (result[window.location.href]) {
+        button.click();
+      }
+    });
   }
 }
 
